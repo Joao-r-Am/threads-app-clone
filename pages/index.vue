3b36c32df7894c91d106b6b2831594f8879f610a
Joao-r-Am <joaoric.amorim@gmail.com>
2023-08-04 01:35:15 -0300

    modal logout

--- pages/index.vue
+++ pages/index.vue
@@ -15,11 +15,24 @@
         },
       ]"
     />
+    <Modal
+      :class="[
+        {
+          'max-h-[100vh] transition-all duration-200 ease-in visible':
+            userStore.isLogoutOverlay,
+        },
+        {
+          'max-h-[0vh] transition-all duration-200 ease-in visible':
+            !userStore.isLogoutOverlay,
+        },
+      ]"
+    />
   </div>
 </template>
 
 <script setup>
 import Vue from 'vue'
+import Modal from '~/components/Modal.vue'
 import CreatePost from '~/components/CreatePost.vue'
 import { useUserStore } from '~/store/user'
 
