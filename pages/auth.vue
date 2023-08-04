fb7d40320740750a2a98018016adb7bd2d96eab2
Joao-r-Am <joaoric.amorim@gmail.com>
2023-08-04 10:10:45 -0300

    check password

--- pages/auth.vue
+++ pages/auth.vue
@@ -8,6 +8,7 @@
       <div class="max-w-[350px] mx-auto px-2 text-white">
         <div class="text-center mb-6 mt-4">Login</div>
         <input
+          v-model="email"
           type="text"
           id="Email"
           placeholder="Telefone, nome de usuário ou email"
@@ -15,11 +16,15 @@
         />
 
         <input
+          v-model="password"
           type="password"
           id="Password"
           placeholder="Senha"
-          class="block w-full px-3 py-2 mb-6 text-white placeholder-white transition duration-100 ease-in-out bg-black border border-gray-300 rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+          class="block w-full px-3 py-2 mb-4 text-white placeholder-white transition duration-100 ease-in-out bg-black border border-gray-300 rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
         />
+        <div v-if="checkInput" class="text-red-500 mb-4">
+          Preencha todos os campos.
+        </div>
         <button
           @click="login"
           class="flex items-center mb-4 justify-center gap-3 p-1.5 w-full border rounded-full text-lg font-semibold"
@@ -44,10 +49,18 @@
   </div>
 </template>
 <script setup>
-import { useRouter } from '@nuxtjs/composition-api'
+import { ref, useRouter } from '@nuxtjs/composition-api'
 const router = useRouter()
 
+const email = ref(null)
+const password = ref(null)
+const checkInput = ref(false)
+
 const login = () => {
-  return router.push('/')
+  if (!email.value || !password.value) {
+    checkInput.value = true
+  } else {
+    return router.push('/')
+  }
 }
 </script>
